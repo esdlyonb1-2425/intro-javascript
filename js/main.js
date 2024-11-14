3357b2099273da1fcd24daddc48af6f02436a145
Pierre Lacognata <pierre.lacognata@mail-esd.com>
2024-11-14 10:59:40 +0100

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,38 @@
+
+
+const monTexte = "coucou je suis du texte"
+
+let choucroute = "une choucroute ca se mange"
+
+let chaineDeCaracteres = "sddf+968ùù*" //string
+let unNombre = 123  //integer (nombre entier
+let unNombreDecimal = 12.5 //   float =  nombre a virgule flottante
+let ilFaitBeau = false
+let lesFritesCEstSalé = true
+
+let unTableau = ["pomme", "poire", "pêche"]
+let unAutreTableau = [
+                            "une bouteille d'huile",
+                            "coucou",
+                            12,
+                            false
+                        ]
+let tableauCompliqué= [
+                            "coucou",
+                            [
+                                "pomme",
+                                "poire"
+                            ],
+                            421
+
+
+
+                        ]
+
+
+
+if(ilFaitBeau){
+    console.log("je sors me promener")
+}else{
+    console.log("je sors pas me promener")
+}

d9e6f92dfd6eba1f9ec4b0dec78431e3aa17a16e
Pierre Lacognata <pierre.lacognata@mail-esd.com>
2024-11-14 11:38:12 +0100

    blah

--- js/main.js
+++ js/main.js
@@ -36,3 +36,30 @@ if(ilFaitBeau){
 }else{
     console.log("je sors pas me promener")
 }
+
+//------------------------------------
+
+let nombreDePommes = 13
+let autreNombreDePommes = 4
+let encoreUnAutreNombreDePommes = "7 pommes"
+let encoreUnAutreAutreNombreDePommes = "9"
+let unPrenom = "luc"
+let unAutrePrenom = "anais"
+console.log("HEY REGARDE PAR ICI")
+console.log(nombreDePommes + autreNombreDePommes)
+console.log(unPrenom + unAutrePrenom)
+console.log(unAutrePrenom + nombreDePommes)
+console.log(encoreUnAutreNombreDePommes + nombreDePommes)
+console.log(encoreUnAutreAutreNombreDePommes + nombreDePommes)
+
+console.log("HEY PAR ICI LA SUITE")
+let totalDePommes = nombreDePommes +autreNombreDePommes
+console.log("Le total de pommes : "+totalDePommes)
+
+let variableDeLuc = "luc"
+console.log(variableDeLuc[1])
+
+let unAutreMot = "champignon"
+//à partir de cette variable-la, créer une autre variable
+//contenant les 5 premieres lettres de la premiere
+
